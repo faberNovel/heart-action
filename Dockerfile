f3c8e9dfb50acc5d1277ca8a737c8cacf3509b2f
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2022-12-11 00:18:56 +0100

    feat: add branding icon
    feat: hide the console output when creating the package.json file

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 # use a Node.js image with Chrome installed, because @fabernovel/heart-greenit requires Chrome installed.
 # use latest Node.js LTS instead of Node.js 14 (which is the version supported by Heart) because of
 # an unresolvable bug with node-gyp during installation with Node.js 14 / NPM 6.
-FROM timbru31/node-chrome:18
+FROM node:18
 
 # set environment variable to make the @fabernovel/heart-greenit module work
-ENV CHROME_PATH=/usr/bin/google-chrome-stable
+# ENV CHROME_PATH=/usr/bin/google-chrome-stable
+# https://github.com/puppeteer/puppeteer/issues/290#issuecomment-322838700
+RUN apt-get update && \
+    apt-get -yq install gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # note: $(echo $1 | xargs) syntax is used to trim the $1 string
 # see https://stackoverflow.com/a/12973694
 
+# name of the directory in which the repository will be cloned.
+# this will be used in git clone command below
+repositoryName="repository"
 # service name that analyze the URL (e.g. greenit)
 analysisService=$(echo $1 | xargs)
 # file path to the JSON configuration used by the analysis service.
@@ -14,7 +17,7 @@ threshold=$(echo $4 | xargs)
 # services names that process the result of the analyze, separated by commas (e.g. slack,bigquery)
 listenerServices=$(echo $5 | xargs)
 
-generate_installable_modules() {
+generate_installable_packages() {
   local packageNamePrefix="@fabernovel/heart-"
   local packageMajorVersionIndicator="@^3.0.0"
   local servicesToInstall="cli $analysisService "
@@ -36,16 +39,24 @@ generate_installable_modules() {
 generate_heart_command() {
   local cliOptions=""
 
-  if [ -n "$file" ]; then cliOptions+=" --file $file"; fi
+  if [ -n "$file" ]; then cliOptions+=" --file $repositoryName/$file"; fi
   if [ -n "$inline" ]; then cliOptions+=" --inline $inline"; fi
   if [ -n "$threshold" ]; then cliOptions+=" --threshold $threshold"; fi
 
   echo $analysisService$cliOptions
 }
+ 
+# if the file input has been been set, we need to have the file available for heart to read it.
+# checks that the repository does not already exist too.
+if [ -n "$file" ] && [[ ! -f $repositoryName ]]; then
+  git clone "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY.git" --branch $GITHUB_REF_NAME $repositoryName
+fi
 
 # install dependencies
-installableModules=$(generate_installable_modules)
-npm install $installableModules
+# create the package.json if it does not already exist, and hide the console output
+if [[ ! -f "package.json" ]]; then npm init -y > /dev/null; fi
+packages=$(generate_installable_packages)
+npm install $packages
 
 # run the heart command
 echo "npx heart $(generate_heart_command)"
