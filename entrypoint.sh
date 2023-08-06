a67978145a8429ec39a624060b35acdeb6ca79f4
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2023-08-06 15:52:25 +0200

    feat: add the verbose mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# generate_heart_command analysisService config threshold except-listeners only-listeners
+# generate_heart_command analysisService config threshold except-listeners only-listeners verbose
 # Generate the heart command from the given arguments.
 generate_heart_command() {
   local cliOptions=" --config $2"
@@ -8,6 +8,7 @@ generate_heart_command() {
   if [ -n "$3" ]; then cliOptions+=" --threshold $3"; fi
   if [ -n "$4" ]; then cliOptions+=" --except-listeners $4"; fi
   if [ -n "$5" ]; then cliOptions+=" --only-listeners $5"; fi
+  if [ -n "$6" ]; then cliOptions+=" --verbose"; fi
 
   echo $1$cliOptions
 }
@@ -28,11 +29,12 @@ threshold=$(trim $3)
 # services names that process the result of the analyze, separated by commas (e.g. slack,bigquery)
 exceptServices=$(trim $4)
 onlyServices=$(trim $5)
+verbose=$(trim $6)
 
 # clone the repository, because we need the configuration file if the provided config is a file.
 # checks that the repository does not already exist too.
 git clone "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY.git" --branch $GITHUB_REF_NAME
 
 # run the heart command
-command=$(generate_heart_command "$analysisService" "$config" "$threshold" "$exceptServices" "$onlyServices")
+command=$(generate_heart_command "$analysisService" "$config" "$threshold" "$exceptServices" "$onlyServices" "$verbose")
 npx heart $command
